d5d4281adc581ad78bfdd85baa05b02563892efd
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2024-03-27 19:34:44 +0530

    feat: upload xls functionality

--- bank-statement-parser/src/App.tsx
+++ bank-statement-parser/src/App.tsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import * as XLSX from 'xlsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState(0)
+
+  const handleFileUpload = (e: any) => {
+    const file = e.target.files[0]
+    const reader: FileReader = new FileReader()
+    reader.readAsBinaryString(file)
+    reader.onload = (e) => {
+      const binaryStr: any = e.target?.result
+      const wb: XLSX.WorkBook = XLSX.read(binaryStr, { type: 'binary' })
+      const wsName: string = wb.SheetNames[0]
+      const ws: XLSX.WorkSheet = wb.Sheets[wsName]
+      const transactions: any[] = XLSX.utils.sheet_to_json(ws)
+      console.log(transactions)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="file" accept=".xls" onChange={(e) => handleFileUpload(e)} />
     </>
   )
 }
