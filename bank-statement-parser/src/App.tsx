81c4528f46ac250c0c7560451d38ab90a7316051
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2024-03-30 15:59:47 +0530

    feat: WIP - calculate withdrawals based on category

--- bank-statement-parser/src/App.tsx
+++ bank-statement-parser/src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import * as XLSX from 'xlsx'
+import { Transaction } from './Transaction'
 
 function App() {
-  const [file, setFile] = useState(0)
+  const [category, setCategory] = useState({ food: 0, travel: 0, creditCardBill: 0, investment: 0, misc: 0, refreshments: 0 })
+
 
   const handleFileUpload = (e: any) => {
     const file = e.target.files[0]
@@ -14,14 +16,49 @@ function App() {
       const wb: XLSX.WorkBook = XLSX.read(binaryStr, { type: 'binary' })
       const wsName: string = wb.SheetNames[0]
       const ws: XLSX.WorkSheet = wb.Sheets[wsName]
-      const transactions: any[] = XLSX.utils.sheet_to_json(ws)
-      console.log(transactions)
+      const transactions: Transaction[] = XLSX.utils.sheet_to_json(ws)
+      parseData(transactions)
     }
+  }
+
+  const parseData = (transactions: Transaction[]) => {
+    transactions.forEach((transaction) => {
+      if (transaction.withdrawals) {
+        let particulars: string = transaction.particulars
+
+        if (particulars.includes('Zomato') || particulars.includes('SWIGGY')) {
+          category.food += parseInt(transaction.withdrawals.replace(',', ''))
+          setCategory(category)
+        }
+        else if (particulars.includes('OLACABS')) {
+          category.travel += parseInt(transaction.withdrawals.replace(',', ''))
+          setCategory(category)
+        }
+        else if (particulars.includes('CRED')) {
+          category.creditCardBill += parseInt(transaction.withdrawals.replace(',', ''))
+          setCategory(category)
+        }
+        else if (particulars.includes('GROWW')) {
+          category.investment += parseInt(transaction.withdrawals.replace(',', ''))
+          setCategory(category)
+        }
+        else if (particulars.includes('zepto')) {
+          category.refreshments += parseInt(transaction.withdrawals.replace(',', ''))
+        }
+        else {
+          category.misc += parseInt(transaction.withdrawals.replace(',', ''))
+        }
+      }
+    })
+    console.log(category)
   }
 
   return (
     <>
       <input type="file" accept=".xls" onChange={(e) => handleFileUpload(e)} />
+
+      <h1>Here's what you've spent:</h1>
+
     </>
   )
 }
---
+++ bank-statement-parser/src/Transaction.ts
@@ -0,0 +1,10 @@
+export interface Transaction {
+    balanceAmount: string
+    chequeNumber: string
+    particulars: string
+    slNo: string
+    transactionDate: string
+    valueDate: string
+    withdrawals: string
+    deposits: string
+}
